93e0005ef2a4a13469fb95ceabbc74ed18d7b36d
Oto Machala <oto.machala@mcon-group.com>
2025-02-05 11:47:54 +0100

    Separate router into own module

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,5 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-
-import Home from '@/ui/Home';
-import AppLayout from '@/ui/AppLayout';
-import Error from '@/ui/Error';
-import Cart from '@/features/cart/Cart';
-import CreateOrder, { action as createOrderAction } from '@/features/order/CreateOrder';
-import Menu, { loader as menuLoader } from '@/features/menu/Menu';
-import Order, { loader as orderLoader } from '@/features/order/Order';
-import { action as updateOrdetAction } from '@/features/order/UpdateOrder';
-
-const router = createBrowserRouter([
-  {
-    element: <AppLayout />,
-    errorElement: <Error />,
-
-    children: [
-      {
-        path: '/',
-        element: <Home />,
-      },
-      {
-        path: '/menu',
-        element: <Menu />,
-        loader: menuLoader,
-        errorElement: <Error />,
-      },
-      {
-        path: '/cart',
-        element: <Cart />,
-      },
-      {
-        path: '/order/new',
-        element: <CreateOrder />,
-        action: createOrderAction,
-      },
-      {
-        path: '/order/:id',
-        element: <Order />,
-        loader: orderLoader,
-        errorElement: <Error />,
-        action: updateOrdetAction,
-      },
-    ],
-  },
-]);
+import { RouterProvider } from 'react-router-dom';
+import { router } from '@/router/router';
 
 function App() {
   return <RouterProvider router={router} />;
